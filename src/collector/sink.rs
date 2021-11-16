361184d224450f72828c4c658841a898a0bfc8ea
jprochazk <honza.spacir@gmail.com>
2021-11-16 22:43:02 +0100

    simplify sink - remove `buf_size` - BufWriter's buffer is re-used by only changing the inner file when the log file is rotated

--- src/collector/sink.rs
+++ src/collector/sink.rs
@@ -1,17 +1,24 @@
 use chrono::{Date, Utc};
 use std::{
-  fs,
+  fs::{self, File},
   io::{self, BufWriter, Write},
-  path::PathBuf,
+  path::{Path, PathBuf},
 };
 
 /// File sink which writes to a new file for each day
 pub struct DailyLogSink {
   log_file_prefix: String,
   log_dir: PathBuf,
-  buf_size: usize,
   date: Date<Utc>,
   file: BufWriter<std::fs::File>,
+}
+
+fn open_log_file(dir: &Path, prefix: &str) -> io::Result<File> {
+  let date = Utc::today().format("%F");
+  fs::OpenOptions::new()
+    .create(true)
+    .append(true)
+    .open(dir.join(format!("{prefix}-{date}.log")))
 }
 
 impl DailyLogSink {
@@ -20,20 +27,11 @@ impl DailyLogSink {
       fs::create_dir_all(&log_dir)?;
     }
     let date = Utc::today();
-    let file = fs::OpenOptions::new()
-      .create(true)
-      .append(true)
-      .open(log_dir.join(format!(
-        "{prefix}-{date}.log",
-        prefix = log_file_prefix,
-        date = date.format("%F")
-      )))
-      .map(|file| BufWriter::with_capacity(buf_size, file))?;
+    let file = open_log_file(&log_dir, &log_file_prefix).map(|file| BufWriter::with_capacity(buf_size, file))?;
 
     Ok(DailyLogSink {
       log_file_prefix,
       log_dir,
-      buf_size,
       date,
       file,
     })
@@ -42,24 +40,12 @@ impl DailyLogSink {
 
 impl Write for DailyLogSink {
   fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
-    // ensure buffer never reaches `buf_size`
-    if self.file.buffer().len() + buf.len() >= self.buf_size {
-      self.file.flush()?;
-    }
     // rotate file every day
     let today = Utc::today();
     if today.signed_duration_since(self.date).num_days() > 0 {
       self.date = today;
       self.file.flush()?;
-      self.file = fs::OpenOptions::new()
-        .create(true)
-        .append(true)
-        .open(self.log_dir.join(format!(
-          "{prefix}-{date}.log",
-          prefix = self.log_file_prefix,
-          date = self.date.format("%F")
-        )))
-        .map(|file| BufWriter::with_capacity(self.buf_size, file))?;
+      *self.file.get_mut() = open_log_file(&self.log_dir, &self.log_file_prefix)?;
     }
     // then actually write
     self.file.write(buf)
