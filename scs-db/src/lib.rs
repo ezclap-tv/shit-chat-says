9b229ca7f466ef8e48d277ad1025fa90b1701434
jprochazk <honza.spacir@gmail.com>
2021-12-12 18:55:53 +0100

    fix `logs::Entry::fetch_all`

--- scs-db/src/lib.rs
+++ scs-db/src/lib.rs
@@ -13,6 +13,11 @@ pub async fn connect(uri: impl Into<ConnString>) -> sqlx::Result<Database> {
 }
 
 pub struct ConnString(String);
+impl<'a> From<&'a str> for ConnString {
+  fn from(v: &'a str) -> Self {
+    Self(v.to_string())
+  }
+}
 impl From<String> for ConnString {
   fn from(v: String) -> Self {
     Self(v)
@@ -24,5 +29,27 @@ impl<'a> From<(&'a str, &'a str, i32, &'a str, Option<&'a str>)> for ConnString 
       Some(pass) => format!("postgres://{host}:{port}/{db}?user={user}&password={pass}"),
       None => format!("postgres://{host}:{port}/{db}?user={user}"),
     })
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[tokio::test]
+  async fn connect_and_fetch_all() {
+    if std::env::var("RUST_LOG").is_err() {
+      std::env::set_var("RUST_LOG", "INFO");
+    }
+    env_logger::init();
+    let db = connect("postgres://localhost:5432/scs?user=postgres&password=root")
+      .await
+      .unwrap();
+    for entry in logs::Entry::fetch_all(&db, "moscowwbish", Some("moscowwbish"), Some("okay%"), None, Some(10))
+      .await
+      .unwrap()
+    {
+      log::info!("{} {} {}", entry.channel(), entry.chatter(), entry.message());
+    }
   }
 }
--- scs-db/src/logs.rs
+++ scs-db/src/logs.rs
@@ -104,20 +104,22 @@ impl Entry {
     Ok(())
   }
 
+  /// Retrieve logs into a `Vec`
+  ///
   /// * channel - exact
   /// * chatter - exact
   /// * pattern - uses `LIKE` for matching, e.g. `%yo%`
   ///   * `%` multi-character wildcard
   ///   * `_` single-character wildcard
-  pub async fn find_all(
+  pub async fn fetch_all<S: Into<String>>(
     executor: impl sqlx::PgExecutor<'_> + Copy,
-    channel: String,
-    chatter: Option<String>,
-    pattern: Option<String>,
-    offset: i32,
-    limit: i32,
+    channel: S,
+    chatter: Option<S>,
+    pattern: Option<S>,
+    offset: Option<i32>,
+    limit: Option<i32>,
   ) -> sqlx::Result<Vec<Entry>> {
-    let mut n = 0;
+    let mut n = 1;
     let mut query = format!("SELECT * FROM logs WHERE channel = ${}\n", inc!(n));
     if chatter.is_some() {
       query += &format!("AND chatter = ${}\n", inc!(n));
@@ -125,19 +127,29 @@ impl Entry {
     if pattern.is_some() {
       query += &format!("AND message LIKE ${}\n", inc!(n));
     }
-    query += &format!("LIMIT {} OFFSET {}", inc!(n), inc!(n));
+    query += &format!(
+      "LIMIT ${} OFFSET ${}",
+      if limit.is_some() {
+        inc!(n).to_string()
+      } else {
+        "ALL".to_string()
+      },
+      inc!(n)
+    );
 
     let mut query = sqlx::query_as::<_, Entry>(&query);
 
-    query = query.bind(&channel);
-    if let Some(chatter) = &chatter {
-      query = query.bind(chatter);
+    query = query.bind(channel.into());
+    if let Some(chatter) = chatter {
+      query = query.bind(chatter.into());
     }
-    if let Some(pattern) = &pattern {
-      query = query.bind(pattern);
+    if let Some(pattern) = pattern {
+      query = query.bind(pattern.into());
     }
-    query = query.bind(limit);
-    query = query.bind(offset);
+    if let Some(limit) = limit {
+      query = query.bind(limit);
+    }
+    query = query.bind(offset.unwrap_or(0));
 
     query.fetch_all(executor).await
   }
