eb31dc4f87cd661af2868df2d9e2557570b4dbcd
optimalstrategy <17456182+OptimalStrategy@users.noreply.github.com>
2021-12-12 14:10:33 -0500

    Revert "add a hack to potentially work around cloudflare's buffering nonsense"
    This reverts commit 268f43653b993b186e083be06448304825413828.

--- scs-ci-api/src/schema.rs
+++ scs-ci-api/src/schema.rs
@@ -21,72 +21,10 @@ pub enum OutputKind {
 pub struct CommandOutput {
   pub output: String,
   pub output_kind: OutputKind,
-  #[cfg(feature = "cloudflare-hack")]
-  #[serde(serialize_with = "cloudflare_hack")]
-  pub cloudflare_hack: (),
-}
-
-impl CommandOutput {
-  pub fn new(output: String, output_kind: OutputKind) -> Self {
-    Self {
-      output,
-      output_kind,
-      #[cfg(feature = "cloudflare-hack")]
-      cloudflare_hack: (),
-    }
-  }
 }
 
 #[derive(serde::Serialize)]
 pub struct CommandResult {
   pub is_success: bool,
   pub status_line: String,
-
-  #[cfg(feature = "cloudflare-hack")]
-  #[serde(serialize_with = "cloudflare_hack")]
-  pub cloudflare_hack: (),
 }
-
-impl CommandResult {
-  pub fn new(is_success: bool, status_line: String) -> Self {
-    Self {
-      is_success,
-      status_line,
-      #[cfg(feature = "cloudflare-hack")]
-      cloudflare_hack: (),
-    }
-  }
-}
-
-#[cfg(feature = "cloudflare-hack")]
-fn cloudflare_hack<S>(_: &(), s: S) -> Result<S::Ok, S::Error>
-where
-  S: serde::Serializer,
-{
-  s.serialize_str(CLOUDFLARE_PADDING)
-}
-
-#[cfg(feature = "cloudflare-hack")]
-macro_rules! rep {
-  ($t:expr, 4) => {
-    concat!($t, $t, $t, $t)
-  };
-  ($t:expr, 16) => {
-    rep!(rep!($t, 4), 4)
-  };
-  ($t:expr, 64) => {
-    rep!(rep!($t, 16), 4)
-  };
-  ($t:expr, 256) => {
-    rep!(rep!($t, 64), 4)
-  };
-  ($t:expr, 1024) => {
-    rep!(rep!($t, 256), 4)
-  };
-  ($t:expr, 4096) => {
-    rep!(rep!($t, 1024), 4)
-  };
-}
-
-#[cfg(feature = "cloudflare-hack")]
-pub const CLOUDFLARE_PADDING: &str = rep!("\u{200B}", 4096);
